9b335092c94cd79b27650d007e2b12186b9e3460
Dr Strange <ifedolapo0001@gmail.com>
2023-05-22 17:54:09 -0700

    moving to vs code

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""returns json data from a given rest api"""
+
+import requests
+import sys
+
+
+if __name__ = "__main__":
+    url = 'https://jsonplaceholder.typicode.com/'
+
+    user = '{}
